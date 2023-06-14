e0db13347e6dc2890d76379d1ddce1791956e125
caglenoah <caglenoah@gmail.com>
2023-06-14 08:36:46 -0600

    added base code

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("circle").onclick = function() {
+    document.getElementById("circle").style.display = "none";
+}
+
+document.getElementById("rounded").onclick = function() {
+    document.getElementById("rounded").style.display = "none";
+}
+
+document.getElementById("square").onclick = function() {
+    document.getElementById("square").style.display = "none";
+}
