79f110a3da08825997924e24acd60772ba0baa9d
caglenoah <caglenoah@gmail.com>
2023-06-14 10:36:05 -0600

    completed js assignment

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-document.getElementById("circle").onclick = function() {
-    document.getElementById("circle").style.display = "none";
+const disappear = (element) => {
+   //console.log("Element was clicked. and the ID is: " + element.className)
+//console.log(element.style)
+   document.getElementById(element.id).style.display = "none"
 }
 
-document.getElementById("rounded").onclick = function() {
-    document.getElementById("rounded").style.display = "none";
-}
 
-document.getElementById("square").onclick = function() {
-    document.getElementById("square").style.display = "none";
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//return console.log(num1 + num2)}
+//const num1 = 70
+//const num2 = 5
+
+//function myFunction() {
+
+//document.getElementById("circle").onclick = function() {
+   // document.getElementById("circle").style.display = "none";}
+
+//document.getElementById("rounded").onclick = function() {
+    //document.getElementById("rounded").style.display = "none";}
+
+//document.getElementById("square").onclick = function() {
+   // document.getElementById("square").style.display = "none";}
